cc44be27e24930f870810768cfd6bf49b493c19b
mingming-tang <mingming.tang@aliyun.com>
2019-08-21 21:41:31 +0800

    201908212141

--- download_data.sh
+++ download_data.sh
@@ -1,7 +1,7 @@
 echo "Creating data directory..."
-#mkdir -p data && cd data
-#mkdir weights
-#mkdir pascal_voc
+mkdir -p data && cd data
+mkdir weights
+mkdir pascal_voc
 
 echo "Downloading Pascal VOC 2012 data..."
 wget http://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar
